caa917ea60c683f3a578de9adf50d0f98e1240d2
Nathan van de Pieterman <nathan.vandepieterman@gmail.com>
2023-11-02 11:21:02 +0100

    about me section done

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ var typed = new Typed('.typed', {
     typeSpeed: 100,
     showCursor: false,
 });
+
+var typed = new Typed('.aboutme', {
+    strings: ["Developer", "Student"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    showCursor: false,
+});
